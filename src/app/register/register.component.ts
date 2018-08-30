9cafcedf606109ec1b886ac477e38dab7e6bbddd
Rajiniks <rajiniks1994@gmail.com>
2018-08-30 19:08:38 +0530

    register and routing added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { NgModule } from "@angular/core";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  RegisterApp: FormGroup;
+  errorText: string;
+  constructor(private fb: FormBuilder) {
+    this.RegisterApp = fb.group({
+      defaultFormEmail: ["", Validators.required],
+      defaultFormPass: ["", [Validators.required, Validators.minLength(8)]]
+    });
   }
 
+  ngOnInit() {}
+
+  login() {
+    if (this.RegisterApp.untouched || this.RegisterApp.invalid) {
+      this.errorText = "Please enter valid login credentials";
+    } else {
+      this.errorText = "";
+      alert("Submission Successful");
+    }
+  }
 }
